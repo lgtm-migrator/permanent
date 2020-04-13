99c86dbfb4634087374d38f0176b07e0e327dd80
张斌 <960339491@qq.com>
2020-04-13 14:52:22 +0800

    init

---
+++ src/main/java/com/qianxunclub/permanent/PermanentApplication.java
@@ -0,0 +1,16 @@
+package com.qianxunclub.permanent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangbin
+ */
+@SpringBootApplication
+public class PermanentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermanentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qianxunclub/permanent/PermanentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qianxunclub.permanent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermanentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
