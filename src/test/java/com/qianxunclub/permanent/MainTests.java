f14c4402b9f96de8a306503905b8ce8bce2d3022
张斌 <960339491@qq.com>
2020-04-13 17:00:09 +0800

    init

--- src/test/java/com/qianxunclub/permanent/MainTests.java
+++ src/test/java/com/qianxunclub/permanent/MainTests.java
@@ -2,12 +2,9 @@ package com.qianxunclub.permanent;
 
 import com.qianxunclub.permanent.service.login.QqService;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = PermanentApplication.class)
 public class MainTests {
 
@@ -17,6 +14,5 @@ public class MainTests {
     @Test
     public void get() {
         System.out.println(qqService.authorizeUrl("aa"));
-
     }
 }
